d22f6991efb400a9f7f7e8abee2922af356a6bce
Ridog <4443035+Ridog@users.noreply.github.com>
2023-03-21 14:48:48 +0100

    function restructuring

--- web_agent.py
+++ web_agent.py
@@ -8,26 +8,23 @@ import Globals
 driver = webdriver.Chrome()
 
 def send_command(command):
+    if driver.current_url != Globals.cueserver_ip:
+        driver.get(Globals.cueserver_ip)
+        time.sleep(1)
+    
     try:
         input_box = driver.find_element(By.ID, 'cmd_s')
     except NoSuchElementException:
         print("Error: Could not find command input box")
         return
-    
-    # Type the command into the input box
-    input_box.send_keys(command)
-    time.sleep(1)
-    
+
     try:
+        # Type the command into the input box
+        input_box.send_keys(command)
+        time.sleep(1)
         # Press Enter to submit the command
         input_box.send_keys(Keys.RETURN)
         time.sleep(0.5)
     except:
         print("Error: Failed to execute command")
 
-# Open the cueserver webpage
-driver.get(Globals.cueserver_ip)
-
-
-
-driver.quit()
