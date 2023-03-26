9aa88d26039c9b6a3bd15df0051c02c3cb433d27
Ridog <4443035+Ridog@users.noreply.github.com>
2023-03-26 11:22:55 +0200

    switched from selinium to a simple UDP socket

--- web_agent.py
+++ web_agent.py
@@ -1,30 +1,16 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import time
 import Globals
-
-# Initialize the browser (this example uses Chrome)          
-driver = webdriver.Chrome()
+import socket
 
 def send_command(command):
-    if driver.current_url != Globals.cueserver_ip:
-        driver.get(Globals.cueserver_ip)
-        time.sleep(1)
-    
-    try:
-        input_box = driver.find_element(By.ID, 'cmd_s')
-    except NoSuchElementException:
-        print("Error: Could not find command input box")
-        return
+    # Create a UDP client socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    try:
-        # Type the command into the input box
-        input_box.send_keys(command)
-        time.sleep(1)
-        # Press Enter to submit the command
-        input_box.send_keys(Keys.RETURN)
-        time.sleep(0.5)
-    except:
-        print("Error: Failed to execute command")
+    # Set the IP address and port number of the server
+    server_address = (Globals.cueserver_ip, 52737)
 
+    # Encode the command string as bytes and send it to the server
+    message = command.encode()
+    client_socket.sendto(message, server_address)
+
+    # Close the socket
+    client_socket.close()
